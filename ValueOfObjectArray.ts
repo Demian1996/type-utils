ee87f37b9125f753436cd6f88d05e057ec399103
wangdengzhi <1303465909@qq.com>
2020-09-24 08:34:39 +0800

    [feature][what]增加场景描述

--- ValueOfObjectArray.ts
+++ ValueOfObjectArray.ts
@@ -3,6 +3,8 @@ import { ValueOf } from 'type-fest';
 /**
 遍历对象数组元素，取出对象元素给定key值的value值，返回联合类型
 @example
+场景描述：页面中维护了一个菜单列表，handle函数中会对key做switch条件语句处理，此时入参中需要将所有menuItem的key值作为联合类型
+在不抽离为枚举类型时，可以用如下方式：
 ```
 import { Mutable } from './Mutable';
 
@@ -19,6 +21,11 @@ const menu = [
 
 
 type A = ValueOfObjectArray<Mutable<typeof menu>, 'key'>;
+// 'edit'|'delete'
+
+function handle(key: A) {
+  switch(key) {...}
+}
 ```
 */
 
